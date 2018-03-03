b33b6005ed4b707ba04751433352725998a7d86c
Angus Moore <angus@ulladullwebdesign.com>
2018-03-04 02:41:40 +1100

    Can resize in all directions :)

--- js/img-crop.js
+++ js/img-crop.js
@@ -1,14 +1,23 @@
 function Crop(el) {
 
     /* Get DOM node from selector */
-    let wrapper = document.querySelector(el)
-    let upload_btn = wrapper.querySelector('.upload-btn')
-    let remove_btn = wrapper.querySelector('.remove-btn')
-    let preview = wrapper.querySelector('.crop-preview')
+    this.element = document.querySelector(el)
+
+    /* Get all other elements */
+    let upload_btn = this.element.querySelector('.upload-btn')
+    let remove_btn = this.element.querySelector('.remove-btn')
+    let input = this.element.querySelector('input[type=file].hidden')
+    let preview = this.element.querySelector('.crop-preview')
     let preview_inner = preview.querySelector('.crop-inner')
-    let input = wrapper.querySelector('input[type=file].hidden')
     let img_el = preview_inner.querySelector('img')
     let crop_overlay = document.createElement('div')
+
+    this.crop_area = {
+        top: 40,
+        bottom: 50,
+        left: 40,
+        right: 400
+    }
 
     /* Open file browser */
     let choose_image = () => {
@@ -31,6 +40,7 @@ function Crop(el) {
             }
             reader.readAsDataURL(input.files[0])
         }
+        _update_offset()
     }
 
     /* REMOVE image preview */
@@ -43,7 +53,7 @@ function Crop(el) {
         input.type = 'file'
         input.classList.add('hidden')
         input.addEventListener('change', show_preview, false)
-        wrapper.querySelector('form').appendChild(input)
+        this.element.querySelector('form').appendChild(input)
     }
 
     var cur_side
@@ -58,19 +68,41 @@ function Crop(el) {
         return false
     }
 
-    var overlay_offset = preview_inner.getBoundingClientRect()
-    let click_move = e => {
-        if (cur_side.index & 1) {
-            // Odd number - top / bottom - bitwise comparison
-            crop_overlay.style.top = (e.pageY - overlay_offset.top) + 'px'
-        } else {
-            // Even number - left / right
-            crop_overlay.style.left = (e.pageX - overlay_offset.left) + 'px'
+    var preview_offset,
+        _update_offset = () => {
+            preview_offset = preview_inner.getBoundingClientRect()
         }
+    setTimeout(_update_offset, 100);
+    let click_move = e => {
+        if (cur_side.index == 1) {
+            // Top
+            let _top = Math.round(e.pageY - preview_offset.top)
+            if (_top < preview_inner.clientHeight - this.crop_area.bottom && _top > 0) {
+                this.crop_area.top = _top
+            }
+        } else if (cur_side.index == 3) {
+            // Bottom
+            let _bottom = Math.round((preview_offset.top + preview_inner.clientHeight) - e.pageY)
+            if (_bottom < preview_inner.clientHeight - this.crop_area.top && _bottom > 0) {
+                this.crop_area.bottom = _bottom
+            }
+        } else if (cur_side.index == 4) {
+            // Left
+            let _left = Math.round(e.pageX - preview_offset.left)
+            if (_left < preview_inner.clientWidth - this.crop_area.right && _left > 0) {
+                this.crop_area.left = _left
+            }
+        } else {
+            // Right
+            let _right = Math.round((preview_offset.left + preview_inner.clientWidth) - e.pageX)
+            if (_right < preview_inner.clientWidth - this.crop_area.left && _right > 0) {
+                this.crop_area.right = _right
+            }
+        }
+        _update_overlay();
     }
 
     let click_up = e => {
-        // console.log(e.offsetX, e.offsetY, 'mouse up', cur_side)
         preview_inner.removeEventListener('mousemove', click_move, false)
     }
 
@@ -79,7 +111,13 @@ function Crop(el) {
         upload_btn.addEventListener('click', choose_image, false)
         remove_btn.addEventListener('click', remove_preview, false)
         input.addEventListener('change', show_preview, false)
-        // preview_inner.addEventListener('mousedown', click_down, false)
+    }
+
+    let _update_overlay = () => {
+        crop_overlay.style.left = this.crop_area.left + 'px';
+        crop_overlay.style.top = this.crop_area.top + 'px';
+        crop_overlay.style.width = (preview_inner.clientWidth - this.crop_area.left - this.crop_area.right) + 'px';
+        crop_overlay.style.height = (preview_inner.clientHeight - this.crop_area.top - this.crop_area.bottom) + 'px';
     }
 
 
@@ -93,15 +131,17 @@ function Crop(el) {
             crop_overlay.appendChild(side)
         }
         crop_overlay.classList.add('crop-overlay')
+
         preview_inner.appendChild(crop_overlay)
         if (!img_el) {
             img_el = document.createElement('img')
             preview_inner.appendChild(img_el)
+        } else {
+            img_el.addEventListener('load', _update_overlay)
         }
         bind_events()
     })()
 
-    this.element = wrapper
     this.choose_image = choose_image
 
     return this
