b7bf77daa266b75817b31ac6621225949154c915
Angus Moore <angus@ulladullwebdesign.com>
2018-03-12 08:11:16 +1100

    Updated demo

--- js/img-crop.js
+++ js/img-crop.js
@@ -3,8 +3,7 @@
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 function Crop(options) {
-    var _this = this,
-        _arguments = arguments;
+    var _this = this;
 
     /* Get DOM node from selector */
     this.element = document.querySelector((typeof options === 'undefined' ? 'undefined' : _typeof(options)) == 'object' ? options.element : options);
@@ -70,14 +69,6 @@ function Crop(options) {
         _this.element.querySelector('form').appendChild(input);
     };
 
-    /* Ajax update preview of processed image */
-    // let update_preview = () => {
-    //     if (!this.preview) return
-    //     fetch(form.action).then((response) => {
-    //         this.preview.innerHtml = response
-    //     })
-    // }
-
     var cur_side;
     var click_down = function click_down(e) {
         cur_side = {
@@ -94,78 +85,82 @@ function Crop(options) {
         preview_offset = preview_inner.getBoundingClientRect();
     };
     setTimeout(_update_offset, 100);
-    var click_move = function click_move(e) {
+    function click_move(e) {
         if (cur_side.index == 1) {
             // Top
             var _top = Math.round(e.pageY - preview_offset.top);
-            if (_top < preview_inner.clientHeight - _this.crop_area.bottom && _top > 0) {
-                _this.crop_area.top = _top;
+            if (_top < preview_inner.clientHeight - this.crop_area.bottom && _top > 0) {
+                this.crop_area.top = _top;
             }
         } else if (cur_side.index == 3) {
             // Bottom
             var _bottom = Math.round(preview_offset.top + preview_inner.clientHeight - e.pageY);
-            if (_bottom < preview_inner.clientHeight - _this.crop_area.top && _bottom > 0) {
-                _this.crop_area.bottom = _bottom;
+            if (_bottom < preview_inner.clientHeight - this.crop_area.top && _bottom > 0) {
+                this.crop_area.bottom = _bottom;
             }
         } else if (cur_side.index == 4) {
             // Left
             var _left = Math.round(e.pageX - preview_offset.left);
-            if (_left < preview_inner.clientWidth - _this.crop_area.right && _left > 0) {
-                _this.crop_area.left = _left;
+            if (_left < preview_inner.clientWidth - this.crop_area.right && _left > 0) {
+                this.crop_area.left = _left;
             }
         } else {
             // Right
             var _right = Math.round(preview_offset.left + preview_inner.clientWidth - e.pageX);
-            if (_right < preview_inner.clientWidth - _this.crop_area.left && _right > 0) {
-                _this.crop_area.right = _right;
+            if (_right < preview_inner.clientWidth - this.crop_area.left && _right > 0) {
+                this.crop_area.right = _right;
             }
         }
         _update_overlay();
-    };
-
-    var start_drag = function start_drag(e) {
-        if (e.target.classList.contains('side')) return;
-        preview_inner.addEventListener('mousemove', drag_crop_area, false);
-    };
+    }
 
     var last_position;
-    var drag_crop_area = function drag_crop_area(e) {
+    function drag_crop_area(e) {
         if (last_position) {
             var deltaX = e.clientX - last_position.x,
                 deltaY = e.clientY - last_position.y;
-            _this.crop_area.left += deltaX;
-            _this.crop_area.right -= deltaX;
-            _this.crop_area.top += deltaY;
-            _this.crop_area.bottom -= deltaY;
+            self.crop_area.left += deltaX;
+            self.crop_area.right -= deltaX;
+            self.crop_area.top += deltaY;
+            self.crop_area.bottom -= deltaY;
             _update_overlay();
         }
         last_position = {
             x: e.clientX,
             y: e.clientY
         };
-    };
+    }
 
-    var _update_overlay = function _update_overlay() {
-        crop_overlay.style.left = _this.crop_area.left + 'px';
-        crop_overlay.style.top = _this.crop_area.top + 'px';
-        crop_overlay.style.width = preview_inner.clientWidth - _this.crop_area.left - _this.crop_area.right + 'px';
-        crop_overlay.style.height = preview_inner.clientHeight - _this.crop_area.top - _this.crop_area.bottom + 'px';
-    };
+    function _update_overlay() {
+        crop_overlay.style.left = self.crop_area.left + 'px';
+        crop_overlay.style.top = self.crop_area.top + 'px';
+        crop_overlay.style.width = preview_inner.clientWidth - self.crop_area.left - self.crop_area.right + 'px';
+        crop_overlay.style.height = preview_inner.clientHeight - self.crop_area.top - self.crop_area.bottom + 'px';
+    }
 
-    var _update_preview = function _update_preview() {
-        fetch('/preview', {}).then(function (response) {
-            response.text().then(function (base64_preview) {
-                if (!_this.preview_img) {
-                    _this.preview_img = document.createElement('img');
-                    _this.preview.appendChild(_this.preview_img);
-                }
-                _this.preview_img.src = 'data:image/jpeg;base64,' + base64_preview;
-            });
+    function _update_preview() {
+        var options = {
+            method: 'POST',
+            body: JSON.stringify(self.crop_area),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        };
+        fetch('/preview', options).then(function (response) {
+            return response.text();
+        }).then(function (base64_preview) {
+            if (!self.preview_img) {
+                self.preview_img = document.createElement('img');
+                self.preview.appendChild(self.preview_img);
+            }
+            self.preview_img.src = 'data:image/jpeg;base64,' + base64_preview;
         });
-    };
+    }
 
     /* Limit calls to a function */
-    var throttle = function throttle(callback, limit) {
+    function throttle(callback, limit) {
+        var _arguments = arguments;
+
         var wait = false;
         return function () {
             if (!wait) {
@@ -176,14 +171,17 @@ function Crop(options) {
                 }, limit);
             }
         };
-    };
+    }
 
     /* Bind events */
-    var bind_events = function bind_events() {
+    function bind_events() {
         upload_btn.addEventListener('click', choose_image, false);
         remove_btn.addEventListener('click', remove_preview, false);
         input.addEventListener('change', show_preview, false);
-        crop_overlay.addEventListener('mousedown', start_drag, false);
+        crop_overlay.addEventListener('mousedown', function (e) {
+            if (e.target.classList.contains('side')) return;
+            preview_inner.addEventListener('mousemove', drag_crop_area, false);
+        }, false);
         document.addEventListener('mouseup', function () {
             last_position = null;
             preview_inner.removeEventListener('mousemove', click_move, false);
@@ -195,7 +193,7 @@ function Crop(options) {
             _update_overlay();
             last_position = null;
         }, 50), false);
-    };
+    }
 
     /* Initialise preview */
     (function () {
